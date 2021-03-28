d4ffab8de0507d5bc8d2468489244145417532e0
roirotoras012 <roirotoras012@gmail.com>
2021-03-28 21:48:33 +0800

    zxc

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,61 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+
+  {
+    path: 'home/about',
+    loadChildren: () => import('./home/about/about.module').then( m => m.AboutPageModule)
+  },
+
+  {
+    path: 'home/services',
+    loadChildren: () => import('./home/services/services.module').then( m => m.ServicesPageModule)
+  },
+
+  {
+    path: 'home/contact',
+    loadChildren: () => import('./home/contact/contact.module').then( m => m.ContactPageModule)
+  },
+
+  {
+    path: 'home/clients',
+    loadChildren: () => import('./home/clients/clients.module').then( m => m.ClientsPageModule)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+  },
+  {
+    path: 'services',
+    loadChildren: () => import('./services/services.module').then( m => m.ServicesPageModule)
+  },
+  {
+    path: 'clients',
+    loadChildren: () => import('./clients/clients.module').then( m => m.ClientsPageModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomePageRoutingModule {}
